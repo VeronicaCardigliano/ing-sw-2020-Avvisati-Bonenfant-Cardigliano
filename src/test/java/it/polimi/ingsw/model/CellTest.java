06a8142538d8092a344bb382245156776357735c
Veronica Cardigliano <veronica@Air-di-Veronica.station>
2020-03-29 12:09:57 +0200

    Initial commit

---
+++ src/test/java/it/polimi/ingsw/model/CellTest.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CellTest {
+    @Test
+    public void provatest (){
+        assertTrue(true);
+    }
+
+}
